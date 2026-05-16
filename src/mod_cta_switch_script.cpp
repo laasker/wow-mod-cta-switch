f3ad620a39ee99abfb6e824a964f78d1bf3e8b70
laasker <reksalx@gmail.com>
2026-05-16 09:12:45 -0300

    Revert "Merge branch 'master' of https://github.com/laasker/wow-mod-cta-switch"
    This reverts commit 4f6ff76fd4c296df1851371c6db9f90090897201, reversing
    changes made to 3140d5fe61e5bb541949fe9aa595819e3189b2a5.

--- src/mod_cta_switch_script.cpp
+++ src/mod_cta_switch_script.cpp
@@ -13,28 +13,28 @@ enum EventIds
     EVENT_CTA_EYE_OF_THE_STORM       = 21,
     EVENT_CTA_STRAND_OF_THE_ANCIENTS = 53,
     EVENT_CTA_ISLE_OF_CONQUEST       = 54,
-    EVENT_HOURLY_BELLS               = 73,
 };
 
 class cta_switch_event_script : public GameEventScript
 {
 public:
-    cta_switch_event_script() : GameEventScript("cta_switch_event_script", {
-        GAMEEVENTHOOK_ON_START,
-        GAMEEVENTHOOK_ON_EVENT_CHECK
-    }) { }
+    cta_switch_event_script() : GameEventScript("cta_switch_event_script") { }
 
     void OnStart(uint16 eventId) override
     {
         if (!sConfigMgr->GetOption<int>("ModCTASwitch.Enable", 0))
+        {
             return;
+        }
 
         switch (eventId)
         {
             case EVENT_CTA_WARSONG_GULCH:
 
                 if (!sConfigMgr->GetOption<int>("ModCTASwitch.SwitchWsg", EVENT_CTA_WARSONG_GULCH))
+                {
                     return;
+                }
 
                 if (sConfigMgr->GetOption<int>("ModCTASwitch.SwitchWsg", EVENT_CTA_WARSONG_GULCH) == eventId)
                 {
@@ -48,7 +48,9 @@ public:
             case EVENT_CTA_ARATHI_BASIN:
 
                 if (!sConfigMgr->GetOption<int>("ModCTASwitch.SwitchAB", EVENT_CTA_ARATHI_BASIN))
+                {
                     return;
+                }
 
                 if (sConfigMgr->GetOption<int>("ModCTASwitch.SwitchAB", EVENT_CTA_ARATHI_BASIN) == eventId)
                 {
@@ -62,7 +64,9 @@ public:
             case EVENT_CTA_ALTERAC_VALLEY:
 
                 if (!sConfigMgr->GetOption<int>("ModCTASwitch.SwitchAV", EVENT_CTA_ALTERAC_VALLEY))
+                {
                     return;
+                }
 
                 if (sConfigMgr->GetOption<int>("ModCTASwitch.SwitchAV", EVENT_CTA_ALTERAC_VALLEY) == eventId)
                 {
@@ -76,7 +80,9 @@ public:
             case EVENT_CTA_EYE_OF_THE_STORM:
 
                 if (!sConfigMgr->GetOption<int>("ModCTASwitch.SwitchEots", EVENT_CTA_EYE_OF_THE_STORM))
+                {
                     return;
+                }
 
                 if (sConfigMgr->GetOption<int>("ModCTASwitch.SwitchEots", EVENT_CTA_EYE_OF_THE_STORM) == eventId)
                 {
@@ -90,7 +96,9 @@ public:
             case EVENT_CTA_STRAND_OF_THE_ANCIENTS:
 
                 if (!sConfigMgr->GetOption<int>("ModCTASwitch.SwitchSota", EVENT_CTA_STRAND_OF_THE_ANCIENTS))
+                {
                     return;
+                }
 
                 if (sConfigMgr->GetOption<int>("ModCTASwitch.SwitchSota", EVENT_CTA_STRAND_OF_THE_ANCIENTS) == eventId)
                 {
@@ -104,7 +112,9 @@ public:
             case EVENT_CTA_ISLE_OF_CONQUEST:
 
                 if (!sConfigMgr->GetOption<int>("ModCTASwitch.SwitchIoc", EVENT_CTA_ISLE_OF_CONQUEST))
+                {
                     return;
+                }
 
                 if (sConfigMgr->GetOption<int>("ModCTASwitch.SwitchIoc", EVENT_CTA_ISLE_OF_CONQUEST) == eventId)
                 {
@@ -115,69 +125,15 @@ public:
                 sGameEventMgr->StopEvent(eventId, true);
                 sGameEventMgr->StartEvent(sConfigMgr->GetOption<int>("ModCTASwitch.SwitchIoc", EVENT_CTA_ALTERAC_VALLEY), true);
                 break;
-            case EVENT_HOURLY_BELLS:
-                if (!sConfigMgr->GetOption<bool>("ModCTASwitch.DailyCTA", false))
-                    return;
-
-                time_t t = time(nullptr);
-                tm* now = localtime(&t);
-
-                // Exclude Friday (5), Saturday (6), Sunday (0), and Monday (1)
-                if (now->tm_wday == 5 || now->tm_wday == 6 || now->tm_wday == 0 || now->tm_wday == 1)
-                    return;
-
-                if (now->tm_hour == 6)
-                {
-                    std::vector<uint16> eventIds = { EVENT_CTA_ALTERAC_VALLEY, EVENT_CTA_ARATHI_BASIN, EVENT_CTA_WARSONG_GULCH,
-                        EVENT_CTA_EYE_OF_THE_STORM, EVENT_CTA_ISLE_OF_CONQUEST, EVENT_CTA_STRAND_OF_THE_ANCIENTS };
-
-                    for (uint16 const& activeEvent : eventIds)
-                    {
-                        if (sGameEventMgr->IsActiveEvent(activeEvent))
-                        {
-                            GameEventMgr::GameEventDataMap const& events = sGameEventMgr->GetEventMap();
-
-                            if (std::size_t(activeEvent) >= events.size())
-                            {
-                                LOG_ERROR("module", "[CTA-Switch]: Error, tried to stop unexisting event. ID: {}", activeEvent);
-                                return;
-                            }
-
-                            GameEventData const& eventData = events[activeEvent];
-
-                            sGameEventMgr->StopEvent(activeEvent, true);
-                            LOG_INFO("module", "[CTA-Switch]: Stopping {} ({})", eventData.Description, activeEvent);
-                        }
-                    }
-                }
-
-                if (!sGameEventMgr->IsActiveEvent(EVENT_CTA_ALTERAC_VALLEY) &&
-                    !sGameEventMgr->IsActiveEvent(EVENT_CTA_ARATHI_BASIN) &&
-                    !sGameEventMgr->IsActiveEvent(EVENT_CTA_WARSONG_GULCH) &&
-                    !sGameEventMgr->IsActiveEvent(EVENT_CTA_EYE_OF_THE_STORM) &&
-                    !sGameEventMgr->IsActiveEvent(EVENT_CTA_ISLE_OF_CONQUEST) &&
-                    !sGameEventMgr->IsActiveEvent(EVENT_CTA_STRAND_OF_THE_ANCIENTS))
-                {
-                    sGameEventMgr->StartEvent(urand(EVENT_CTA_WARSONG_GULCH, EVENT_CTA_EYE_OF_THE_STORM), true);
-                }
-
-                break;
         }
     }
 
     void OnEventCheck(uint16 /*eventId*/) override
     {
         if (!sConfigMgr->GetOption<int>("ModCTASwitch.Enable", 0))
+        {
             return;
-
-        if (sConfigMgr->GetOption<bool>("ModCTASwitch.DailyCTA", false))
-            return;
-
-        time_t t = time(nullptr);
-        tm* now = localtime(&t);
-
-        if (now->tm_wday != 2 /* Tuesday */)
-            return;
+        }
 
         std::vector<uint16> eventIds = { EVENT_CTA_ALTERAC_VALLEY, EVENT_CTA_ARATHI_BASIN, EVENT_CTA_WARSONG_GULCH,
             EVENT_CTA_EYE_OF_THE_STORM, EVENT_CTA_ISLE_OF_CONQUEST, EVENT_CTA_STRAND_OF_THE_ANCIENTS };
@@ -186,18 +142,24 @@ public:
         {
             if (sGameEventMgr->IsActiveEvent(activeEvent))
             {
-                GameEventMgr::GameEventDataMap const& events = sGameEventMgr->GetEventMap();
+                time_t t = time(nullptr);
+                tm* now = localtime(&t);
 
-                if (std::size_t(activeEvent) >= events.size())
+                if (now->tm_wday == 2 /* Tuesday */)
                 {
-                    LOG_ERROR("module", "[CTA-Switch]: Error, tried to stop unexisting event. ID: {}", activeEvent);
-                    return;
+                    GameEventMgr::GameEventDataMap const& events = sGameEventMgr->GetEventMap();
+
+                    if (std::size_t(activeEvent) >= events.size())
+                    {
+                        LOG_ERROR("module", "[CTA-Switch]: Error, tried to stop unexisting event. ID: {}", activeEvent);
+                        return;
+                    }
+
+                    GameEventData const& eventData = events[activeEvent];
+
+                    sGameEventMgr->StopEvent(activeEvent, true);
+                    LOG_INFO("module", "[CTA-Switch]: Stopping {} ({})", eventData.description, activeEvent);
                 }
-
-                GameEventData const& eventData = events[activeEvent];
-
-                sGameEventMgr->StopEvent(activeEvent, true);
-                LOG_INFO("module", "[CTA-Switch]: Stopping {} ({})", eventData.Description, activeEvent);
             }
         }
     }
